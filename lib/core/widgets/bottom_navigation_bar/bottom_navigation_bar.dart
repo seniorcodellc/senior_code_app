e3d430bd75fd89a081556373b75c5a86e6f8559c
axah710 <ahmed36085361@gmail.com>
2024-09-28 18:49:53 +0300

    Add initial implementation of CustomBottomNavigationBar widget
    This commit introduces the initial implementation of the `CustomBottomNavigationBar` widget, which is designed to manage navigation between different screens in the app. The widget includes a `BottomNavigationBarBody` that handles the visual representation of the navigation bar and a callback function to update the selected index.
    
    The `CustomBottomNavigationBar` widget maintains a list of screens corresponding to each tab in the navigation bar. The `IndexedStack` widget is used to keep the currently selected tab active, ensuring a smooth user experience when switching between tabs.
    
    Additionally, this commit includes placeholder screens for `ProfileScreen`, `ReviewsScreen`, and `ChatScreen`, which are currently displaying simple text placeholders. These screens will be further developed in subsequent commits.
    
    The code is currently commented out with a note indicating that it will be changed in future commits. This is to allow for iterative development and refinement of the navigation bar functionality.

--- lib/core/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/core/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -84,3 +84,94 @@ class ChatScreen extends StatelessWidget {
     );
   }
 }
+
+
+//! This code will be changed in the next commits of the project...
+// import 'package:flutter/material.dart';
+// import 'package:senior_code_app/exports.dart';
+// import '../../../featuers/home/presentation/screens/home_screen.dart';
+// import '../../../featuers/reservation/presentation/screen/reservation_screen.dart';
+// import 'bottom_navigation_bar_body.dart';
+
+// class CustomBottomNavigationBar extends StatefulWidget {
+//   const CustomBottomNavigationBar({super.key});
+
+//   @override
+//   State<CustomBottomNavigationBar> createState() =>
+//       _CustomBottomNavigationBarState();
+// }
+
+// class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+//   int selectedIndex = 2; // Default to Home
+
+//   // List of screens for each tab
+//   final List<Widget> screens = [
+//     const ProfileScreen(),
+//     const ReservationScreen(),
+//     const HomeScreen(),
+//     const ReviewsScreen(),
+//     const ChatScreen(),
+//   ];
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       selectedIndex = index;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: IndexedStack(
+//         index: selectedIndex, // Keep the currently selected tab active
+//         children: screens, // List of your screens
+//       ),
+//       bottomNavigationBar: BottomNavigationBarBody(
+//         selectedIndex: selectedIndex,
+//         onItemTapped: _onItemTapped, // Pass the callback function
+//       ),
+//     );
+//   }
+// }
+
+// // Profile Screen
+// class ProfileScreen extends StatelessWidget {
+//   const ProfileScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Center(
+//       child: Text(
+//         'Profile Screen',
+//       ),
+//     );
+//   }
+// }
+
+// // Reviews Screen
+// class ReviewsScreen extends StatelessWidget {
+//   const ReviewsScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Center(
+//       child: Text(
+//         'Reviews Screen',
+//       ),
+//     );
+//   }
+// }
+
+// // Chat Screen
+// class ChatScreen extends StatelessWidget {
+//   const ChatScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Center(
+//       child: Text(
+//         'Chat Screen',
+//       ),
+//     );
+//   }
+// }
